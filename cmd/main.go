9921d8277e922fea3f836e0305eab3ede0d32bd9
Artem Zaglubotskii <mr_a953@mail.ru>
2021-01-04 20:00:39 +0000

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"gitlab.com/inview-team/raptor_team/worker/internal/cameras"
+	"gitlab.com/inview-team/raptor_team/worker/internal/rabbit"
 	"gitlab.com/inview-team/raptor_team/worker/internal/logger"
 )
 
 func main() {
 	logger.Info.Print("Worker start")
 	var url = "rtsp://user:qwerty1234@10.10.0.136:5506/cam/realmonitor?channel=1&subtype=0"
-	cameras.WorkWithVideo(url, ":8080")
+	rabbit.RabbitRun()
 }
